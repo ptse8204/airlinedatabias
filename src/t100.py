d3068ceeb6d66de484ce1c813d7d57a6243fc38c
ptse8204 <88422737+ptse8204@users.noreply.github.com>
2023-02-10 17:51:04 -0800

    first gitpy commit

--- src/t100.py
+++ src/t100.py
@@ -69,7 +69,7 @@ def t100_combined(ticket_df, coupon_df, t100_path):
   coupon_t100 = matching_coupon(useful_cols(import_T100(t100_path)), pre_t100)
   del pre_t100
   coupon_df_reduced = coupon_t100[['ItinID','SeqNum', 'Coupons', 'Year', 
-                                 'Quarter', 'OriginCityMarketID', 'Origin', 'OriginState'
+                                 'Quarter', 'OriginCityMarketID', 'Origin', 'OriginState',
                                  'DestCityMarketID', 'Dest', 
                                  'DestState', 'CouponGeoType', 'FareClass', 'Distance',
                                  'DistanceGroup']].rename(columns={'Distance': 'CouponDistance',
