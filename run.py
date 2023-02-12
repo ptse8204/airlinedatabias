c75581ee3c1c164cf0bc3ea7542594ad5f68f10b
Joseph Perez <81608393+JosephPerez4@users.noreply.github.com>
2023-02-12 12:39:29 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[8]:
-
-
+import sys
 import pandas as pd
-import bias_mitigation_analysis
-ticket=pd.read_csv("Origin_and_Destination_Survey_DB1BTicket_2022_1.csv",nrows=1000000)
-income=pd.read_csv("median_income_cityID.csv")
-race=pd.read_csv("race_id.csv")
-market=pd.read_csv("Origin_and_Destination_Survey_DB1BMarket_2022_1.csv",nrows=1000000)
+import src.bias_mitigation_analysis
 
-
-# In[10]:
-
-
-bias_mitigation_analysis.bias_mitigation(ticket,market, race, income)
-
-
-# In[ ]:
-
-
-
-
+if __name__ == "__main__":
+    if len(sys.argv)>1:
+        x = sys.argv[1]
+        if x =="test":
+            #this needs to use the test data instead
+            print("test data")
+        else:
+            ticket=pd.read_csv("Origin_and_Destination_Survey_DB1BTicket_2022_1.csv",nrows=1000000)
+            income=pd.read_csv("median_income_cityID.csv")
+            race=pd.read_csv("race_id.csv")
+            market=pd.read_csv("Origin_and_Destination_Survey_DB1BMarket_2022_1.csv",nrows=1000000)    
+            bias_mitigation_analysis.bias_mitigation(ticket,market, race, income)
+    else:
+        ticket=pd.read_csv("Origin_and_Destination_Survey_DB1BTicket_2022_1.csv",nrows=1000000)
+        income=pd.read_csv("median_income_cityID.csv")
+        race=pd.read_csv("race_id.csv")
+        market=pd.read_csv("Origin_and_Destination_Survey_DB1BMarket_2022_1.csv",nrows=1000000)    
+        bias_mitigation_analysis.bias_mitigation(ticket,market, race, income)
