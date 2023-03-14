a31613ffc1395c40f527856b8c978c0d52c8ad58
Joseph Perez <81608393+JosephPerez4@users.noreply.github.com>
2023-03-14 09:59:19 -0700

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,8 @@
 # coding: utf-8
 import sys
 import pandas as pd
-import src.bias_mitigation_analysis
+import src.farepermile_biasmitigation
+import src.fareclass_analysis
 
 if __name__ == "__main__":
     if len(sys.argv)>1:
@@ -10,21 +11,33 @@ if __name__ == "__main__":
         if x =="test":
             #this needs to use the test data instead
             print("test data")
+            test=True
+            print("Bias Analysis")
             ticket=pd.read_csv("test/testdata/test_ticket.csv",nrows=1000000)
             income=pd.read_csv("test/testdata/test_income.csv")
             race=pd.read_csv("test/testdata/test_race.csv")
             market=pd.read_csv("test/testdata/test_market.csv",nrows=1000000) 
             coupon=pd.read_csv("test/testdata/test_coupon.csv",nrows=1000000)
-            src.bias_mitigation_analysis.bias_mitigation(ticket,market, race, income)
+            src.farepermile_biasmitigation.fpm_bias_mitigation(ticket,coupon,race,test)
+            print("Fareclass Model")
+            src.fareclass_analysis.fareclass_analysis(coupon, ticket,race)
         else:
+            test=False
+            print("Bias Analysis")
             ticket=pd.read_csv("Origin_and_Destination_Survey_DB1BTicket_2022_1.csv",nrows=1000000)
             income=pd.read_csv("median_income_cityID.csv")
             race=pd.read_csv("race_id.csv")
             market=pd.read_csv("Origin_and_Destination_Survey_DB1BMarket_2022_1.csv",nrows=1000000)    
-            src.bias_mitigation_analysis.bias_mitigation(ticket,market, race, income)
+            src.farepermile_biasmitigation.fpm_bias_mitigation(ticket,coupon,race,test)
+            print("Fareclass Model")
+            src.fareclass_analysis.fareclass_analysis(coupon, ticket,race)
     else:
+        test=False
+        print("Bias Analysis")
         ticket=pd.read_csv("Origin_and_Destination_Survey_DB1BTicket_2022_1.csv",nrows=1000000)
         income=pd.read_csv("median_income_cityID.csv")
         race=pd.read_csv("race_id.csv")
         market=pd.read_csv("Origin_and_Destination_Survey_DB1BMarket_2022_1.csv",nrows=1000000)    
-        src.bias_mitigation_analysis.bias_mitigation(ticket,market, race, income)
+        src.farepermile_biasmitigation.fpm_bias_mitigation(ticket,coupon,race,test)
+        print("Fareclass Model")
+        src.fareclass_analysis.fareclass_analysis(coupon, ticket,race)
